684b14c05e92ff0a8f49068ffd014cdda3bd0273
Soham Donode <sohamdonode001@gmail.com>
2026-04-04 15:52:08 +0530

    setup basic sign up and login pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const manrope = Manrope({
   variable: "--font-manrope",
@@ -22,7 +23,7 @@ export default function RootLayout({
       lang="en"
       className={`${manrope.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col ">  <ClerkProvider>{children}</ClerkProvider></body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
-import Image from "next/image";
+"use client"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@clerk/nextjs";
+import { UserButton, UserProfile } from "@clerk/react";
 
 export default function Home() {
+  const { userId} = useAuth(); 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>APNA PROJECT HEHEHE 🤣</h1>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4 text-center">
+      <div className="space-y-8">
+        <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-6xl">
+          Parasbhau 👮🏻
+        </h1>
+
+        {
+          (!userId) ? (
+          <div className="flex justify-center gap-4">
+          <Button asChild variant="outline">
+            <Link href="/sign-up">Sign Up</Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link href="/sign-in">Sign In</Link>
+          </Button>
+        </div>
+        ) : (
+          <div className="flex justify-center gap-4">
+            <UserButton/>
+          </div>
+        )
+        }
+        
+        
+      </div>
+    </main>
   );
 }
